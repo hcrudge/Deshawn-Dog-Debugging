7db71b6e240470f6c25dcc35d4da5f0fc144833e
hcrudge <hcrudge1@gmail.com>
2021-06-08 13:30:43 -0500

    Explorer Who Walks Whom

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,42 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
 
 export const RegisteredPets = () => {
+
+    document.addEventListener(
+        "click",
+        (event) => {
+    
+            if (event.target.id.startsWith("pet")) {
+                const [, petId] = event.target.id.split("--") 
+    
+                const petIdNumber = parseInt(petId) 
+                for (const pet of pets) {
+                    if ( pet.id === petIdNumber) {
+                        const petWalker = pet.walkerId
+                        
+                        let assignedWalker = 0;
+            
+                        for (const walker of walkers) {
+                            if (walker.id === petWalker) {
+                                assignedWalker = walker.name
+           
+                            } 
+                        }
+        window.alert(`${event.target.innerText} is being walked by ${assignedWalker}.`)
+                    }   
+                }   
+            }
+        }
+    )
+
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
